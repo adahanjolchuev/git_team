69c61ec701c3b38f04710f38288771c8786f3faa
adahan <adahanjolchuev@gmail.com>
2024-02-16 16:53:12 +0600

    gitteam

---
+++ src/companents/Header/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+  return (
+    <div>
+      <h2>tfygjkl;asdfghjkl</h2>
+      <h2>tfygjkl;asdfghjkl</h2>
+      <h2>tfygjkl;asdfghjkl</h2>
+      <button>dsfghjgh</button>
+      <a href="#">sdfghjg</a>
+    </div>
+  );
+}
+
+export default Header;
